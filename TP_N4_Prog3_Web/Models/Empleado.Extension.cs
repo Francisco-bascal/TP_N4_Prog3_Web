53f03a1b9e0aca0e54009ed058e416bd3536dcc7
Bascal Francisco <franbascal@gmail.com>
2026-07-01 22:53:11 -0300

    Eliminación de espacios vacíos.

--- TP_N4_Prog3_Web/Models/Empleado.Extension.cs
+++ TP_N4_Prog3_Web/Models/Empleado.Extension.cs
@@ -10,8 +10,8 @@ namespace TP_N4_Prog3_Web.Models
             get 
             {
                 //if (this.CantidadHijos >= 4) return false;
-                if (this.Estado.Equals(true)) return false;
-                if (this.FechaIngreso < DateOnly.FromDateTime(DateTime.Now.AddYears(-5))) return false;
+                if (this.Estado.Equals(true)) return false; //Si está activo no se puede eliminar 
+                if (this.FechaIngreso < DateOnly.FromDateTime(DateTime.Now.AddYears(-5))) return false; //Si su antiguedad es mayor a 5 años no se puede eliminar
                 return true;
             } 
         }
